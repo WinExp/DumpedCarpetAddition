f310dfbe6bebeaeec4ab89b457cb8b05a36d21a3
WinExp <1450422389@qq.com>
2024-10-30 18:59:08 +0800

    Port to 1.21.1

--- src/main/java/com/github/winexp/dumpedcarpetaddition/carpet/ModSettings.java
+++ src/main/java/com/github/winexp/dumpedcarpetaddition/carpet/ModSettings.java
@@ -6,12 +6,8 @@ public class ModSettings {
     private static class Categories {
         public static final String EXPERIMENTAL = "experimental";
         public static final String BUGFIX = "bugfix";
-        public static final String FEATURE = "feature";
     }
 
     @Rule(categories = {Categories.EXPERIMENTAL, Categories.BUGFIX})
     public static boolean largeOakGenerationFix = false;
-
-    @Rule(categories = {Categories.EXPERIMENTAL, Categories.FEATURE})
-    public static boolean stringDuperReintroduced = false;
 }
