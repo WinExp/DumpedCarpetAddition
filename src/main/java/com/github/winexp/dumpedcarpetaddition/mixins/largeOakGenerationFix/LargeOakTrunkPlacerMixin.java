6a3b4cfd0fb98f8efbf941a1bc5a3bb80b8cc6bb
WinExp <1450422389@qq.com>
2026-04-19 12:49:09 +0800

    feat: support Minecraft 26.1

--- src/main/java/com/github/winexp/dumpedcarpetaddition/mixins/largeOakGenerationFix/LargeOakTrunkPlacerMixin.java
+++ src/main/java/com/github/winexp/dumpedcarpetaddition/mixins/largeOakGenerationFix/LargeOakTrunkPlacerMixin.java
@@ -2,18 +2,18 @@ package com.github.winexp.dumpedcarpetaddition.mixins.largeOakGenerationFix;
 
 import com.github.winexp.dumpedcarpetaddition.carpet.ModSettings;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.gen.trunk.LargeOakTrunkPlacer;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.FancyTrunkPlacer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(LargeOakTrunkPlacer.class)
+@Mixin(FancyTrunkPlacer.class)
 public abstract class LargeOakTrunkPlacerMixin {
-    @ModifyArg(method = "generate", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/BlockPos;<init>(III)V"), index = 1)
-    private int modifyBranchStartY(int u, @Local(ordinal = 6) double t, @Local(ordinal = 5) int m) {
+    @ModifyArg(method = "placeTrunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;<init>(III)V"), index = 1)
+    private int modifyBranchStartY(int y, @Local(name = "branchHeight") double branchHeight, @Local(name = "trunkTop") int trunkTop) {
         if (ModSettings.largeOakGenerationFix)
-            return t > (double) m ? m : MathHelper.floor(t);
-        else return u;
+            return branchHeight > (double) trunkTop ? trunkTop : Mth.floor(branchHeight);
+        else return y;
     }
 }
--- src/main/java/com/github/winexp/dumpedcarpetaddition/mixins/largeOakGenerationFix/TripwireHookBlockMixin.java
+++ src/main/java/com/github/winexp/dumpedcarpetaddition/mixins/largeOakGenerationFix/TripwireHookBlockMixin.java
@@ -3,23 +3,22 @@ package com.github.winexp.dumpedcarpetaddition.mixins.largeOakGenerationFix;
 import com.github.winexp.dumpedcarpetaddition.carpet.ModSettings;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.TripwireHookBlock;
+import net.minecraft.world.level.block.TripWireHookBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Slice;
 
-@Mixin(TripwireHookBlock.class)
+@Mixin(TripWireHookBlock.class)
 public abstract class TripwireHookBlockMixin {
     @WrapOperation(
-            method = "update",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"),
+            method = "calculateState",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Ljava/lang/Object;)Z"),
             slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;", ordinal = 1)
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;", ordinal = 1)
             )
     )
-    private static boolean reintroduceStringDuper(BlockState instance, Block block, Operation<Boolean> original) {
+    private static boolean reintroduceStringDuper(BlockState instance, Object block, Operation<Boolean> original) {
         return ModSettings.stringDuperReintroduced || original.call(instance, block);
     }
 }
